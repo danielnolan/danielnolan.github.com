585991f78af6b0798777a0e5815893f0d7fb6721
Daniel Nolan <danielanolan@gmail.com>
2014-09-06 18:03:00 -0400

    Update travis build script

--- _scripts/travis_build.sh
+++ _scripts/travis_build.sh
@@ -8,11 +8,11 @@ git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
 mkdir tmp
-cp _site/. tmp
+cp /_site/. tmp
 cd tmp
 git init
 git add -f .
 git add tags -f
-git remote add origin https://github.com/danielnolan/danielnolan.github.com.git
+git remote add origin https://${GH_TOKEN}:@github.com
 git commit -m "Updated site after successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 git push -fq origin master > /dev/null
