13d709c3ea4706ae6be51d7f274421ea09837552
Daniel Nolan <danielanolan@gmail.com>
2014-09-06 17:12:16 -0400

    Updating travis build

---
+++ scripts/travis_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+bundle exec jekyll build
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+
+mkdir tmp
+cp _site/. tmp
+git init
+git add -f .
+git add tags -f
+git commit -m "Updated site after successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git push -fq origin master > /dev/null
